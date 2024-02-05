cd3d5cab175823926c702ce778559e2dcf343f73
bellusfennec <bellusfennec@gmail.com>
2024-02-05 15:31:07 +0300

    first commit

---
+++ graphql/schema.js
@@ -0,0 +1,44 @@
+const { gql } = require("apollo-server-lambda");
+
+const typeDefs = gql`
+  type Image {
+    source: String # Url scalar
+    description: String
+    thumbnailSource(width: Int, height: Int): String # Url scalar
+  }
+
+  enum Currency {
+    EUR
+    CHF
+    USD
+  }
+
+  type Price {
+    amount: Int
+    currency: Currency
+  }
+
+  type Product  {
+    id: ID!
+    name: String
+    description: String
+    image: Image
+    brand: String
+    price: Price
+  }
+
+  type Query {
+    product(id: ID!): Product
+  }
+`;
+
+const resolvers = {};
+
+const mocks = {};
+
+module.exports = {
+  typeDefs,
+  resolvers,
+  mocks,
+  mockEntireSchema: true
+};
---
+++ local.js
@@ -0,0 +1,14 @@
+const { ApolloServer } = require("apollo-server");
+
+const { typeDefs, resolvers, mocks } = require("./graphql/schema");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  mocks,
+  mockEntireSchema: false
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
